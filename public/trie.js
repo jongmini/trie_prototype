92e846f5b74dd43906d47218aedf7f38bfa49798
jongmini <jongmin.choi@gmail.com>
2014-03-07 14:56:16 -0800

    learn function is working

--- public/trie.js
+++ public/trie.js
@@ -16,13 +16,32 @@ Trie.prototype.learn = function(word, index){
   // A word does not necessarily end at a leaf.
   // You must mark nodes which are the ends of words,
   // so that the words can be reconstructed later.
+
+  index = index || 0;
+  
+  if(this.characters[word[index]]){
+    this.characters[word[index]].learn(word,index+1);
+  } else {
+    if(index === word.length){
+    this.isWord = true;
+    } else {
+      this.characters[word[index]] = new Trie();
+      this.characters[word[index]].learn(word,index+1);
+    }
+  }
+  
 };
+
 
 Trie.prototype.getWords = function(words, currentWord){
   // This function will return all the words which are
   // contained in this Trie.
   // it will use currentWord as a prefix,
   // since a Trie doesn't know about its parents.
+
+  
+  
+
 };
 
 Trie.prototype.find = function(word, index){
